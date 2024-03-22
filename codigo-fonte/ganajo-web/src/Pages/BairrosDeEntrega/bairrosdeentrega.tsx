0db278f0a656d59fd87a78a3220a4b1cd6723e4a
Mikaelly Carneiro <mikaellyscarneiro@gmail.com>
2024-03-21 23:44:39 -0300

    fix: Ajusta tela bairro

--- codigo-fonte/ganajo-web/src/Pages/BairrosDeEntrega/bairrosdeentrega.tsx
+++ codigo-fonte/ganajo-web/src/Pages/BairrosDeEntrega/bairrosdeentrega.tsx
@@ -1,11 +1,9 @@
-// BairrosDeEntrega.tsx
-
 import React, { useCallback, useEffect, useState } from 'react';
 import { Container, Row, Col, Form, Button, Modal, InputGroup, FormControl } from 'react-bootstrap';
 import { MdOutlineSearch, MdSearch } from 'react-icons/md';
 import { Bairro } from '../../DTOs/Bairro.ts';
 import generateBairros from './Components/data.ts';
-import './bairrosdeentrega.module.scss';
+import './bairrosdeentrega.css';
 import BairroComponent from './Components/bairrocomponent.tsx';
 
 const BairrosDeEntrega: React.FC = () => {
@@ -79,8 +77,8 @@ const BairrosDeEntrega: React.FC = () => {
 
   return (
     <Container fluid className="bairros-container">
-      <Row className="search-row">
-        <Col  className="search-col">
+      <Row className={'search-row'}>
+        <Col  className={'search-col'}>
           <InputGroup>
             <FormControl
               type="text"
@@ -94,72 +92,73 @@ const BairrosDeEntrega: React.FC = () => {
           </InputGroup>
         </Col>
       </Row>
-      <Row className="add-bairro-row">
-        <Col xs={12} className="add-bairro-col">
+      <Row className={'add-bairro-row'}>
+        <Col xs={12} className={'add-bairro-col'}>
           <InputGroup>
             <FormControl
               type="text"
               placeholder="Novo bairro..."
               value={newBairroName}
               onChange={(e) => setNewBairroName(e.target.value)}
-            />
-            <Button onClick={handleAddBairro} variant="outline-secondary" className="add-button">Adicionar</Button>
-          </InputGroup>
-        </Col>
-      </Row>
-      <Row className="card-row">
-        {screenBairros.map((bairro) => (
-          <Col key={bairro.Id} xs={12} md={6} lg={4}>
-            <BairroComponent
-              bairro={bairro}
-              onEdit={() => handleShowModal(bairro)}
-              onDelete={() => {
-                setSelectedBairro(bairro);
-                setShowDeleteModal(true);
-              }}
-            />
+            />            <Button onClick={handleAddBairro} variant="outline-secondary" className={'add-button'}>Adicionar</Button>
+            </InputGroup>
           </Col>
-        ))}
-      </Row>
-      <Modal show={showModal} onHide={handleCloseModal} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Editar Bairro</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Control
-            type="text"
-            placeholder="Novo nome do bairro..."
-            value={editedBairroName}
-            onChange={(e) => setEditedBairroName(e.target.value)}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Cancelar
-          </Button>
-          <Button variant="primary" onClick={handleSaveChanges}>
-            Salvar
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Confirmar Exclusão</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Tem certeza que deseja excluir o bairro "{selectedBairro?.Nome}"?
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>
-            Cancelar
-          </Button>
-          <Button variant="danger" onClick={handleDeleteBairro}>
-            Excluir
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Container>
-  );
-};
-
-export default BairrosDeEntrega;
+        </Row>
+        <Row className={'card-row'}>
+          {screenBairros.map((bairro) => (
+            <Col key={bairro.Id} xs={12} sm={6} md={4} lg={3}> 
+              <BairroComponent
+                bairro={bairro}
+                onEdit={() => handleShowModal(bairro)}
+                onDelete={() => {
+                  setSelectedBairro(bairro);
+                  setShowDeleteModal(true);
+                }}
+              />
+            </Col>
+          ))}
+        </Row>
+        <Modal show={showModal} onHide={handleCloseModal} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Editar Bairro</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Control
+              type="text"
+              placeholder="Novo nome do bairro..."
+              value={editedBairroName}
+              onChange={(e) => setEditedBairroName(e.target.value)}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Cancelar
+            </Button>
+            <Button variant="primary" onClick={handleSaveChanges}>
+              Salvar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Confirmar Exclusão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Tem certeza que deseja excluir o bairro "{selectedBairro?.Nome}"?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>
+              Cancelar
+            </Button>
+            <Button variant="danger" onClick={handleDeleteBairro}>
+              Excluir
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    );
+  };
+  
+  export default BairrosDeEntrega;
+  
+           
