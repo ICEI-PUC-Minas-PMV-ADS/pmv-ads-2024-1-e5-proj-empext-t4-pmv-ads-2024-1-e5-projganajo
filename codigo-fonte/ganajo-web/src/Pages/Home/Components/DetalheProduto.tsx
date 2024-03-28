1701d0720a207da035b2665f74bc70e422af559d
Bruno <bruno.ruas2@gmail.com>
2024-03-27 23:15:31 -0400

    feat: Ajustes no card de detalhe do item

--- codigo-fonte/ganajo-web/src/Pages/Home/Components/DetalheProduto.tsx
+++ codigo-fonte/ganajo-web/src/Pages/Home/Components/DetalheProduto.tsx
@@ -11,15 +11,20 @@ interface DetalheProdutoComponentProps {
 
 export const DetalheProdutoComponent = ({ Produto, onClick }: DetalheProdutoComponentProps) => {
     return (
-        <div className={styles.modalBackground} onClick={onClick}>
+        <div className={styles.modalBackground}>
             <div className={styles.container}>
-                <div>
+                <div className={styles.leftColumn}>
+                    <img className={styles.imgDetalhe} src={Produto.Imagem} alt={Produto.Nome} />
                     <h1 className={styles.nomeStyle}>{formatStringLimit(Produto.Nome, 0, 70, "...")}</h1>
-                    <h3 className={styles.descricaoStyle}>{formatStringLimit(Produto.Descricao, 0, 70, "...")}</h3>
-                    <h2 className={styles.valorStyle}>A partir de {formatValue(Produto.Valor, 2, "R$")}</h2>
+                    <h3 className={styles.descricaoStyle}>{formatStringLimit(Produto.Descricao, 0, 999, "...")}</h3>
                 </div>
-                <div>
-                    <img src={Produto.Imagem} alt={Produto.Nome} />
+                <div className={styles.rightColumn}>
+                    <div className={styles.observacaoItem}>
+                        <input type="text" className={styles.inputObservacao} placeholder="Adicione uma observação" />
+                    </div>
+                    <div>
+                        <button onClick={onClick} className={styles.buttonPedir}>Acrescentar {formatValue(Produto.Valor, 2, "R$")}</button>
+                    </div>
                 </div>
             </div>
         </div>
